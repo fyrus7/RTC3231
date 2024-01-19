b019568e54b806c728aeeee5b22c945425f4b753
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2024-01-20 01:25:05 +0800

    Update timestamp.ino

--- examples/timestamp/timestamp.ino
+++ examples/timestamp/timestamp.ino
@@ -8,16 +8,12 @@
 ** Last Edit:
 */
 
-#include "RTClib.h"
+#include "RTClib2.h"
 
 RTC_DS1307 rtc;
 
 void setup () {
   Serial.begin(57600);
-
-#ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
-#endif
 
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
